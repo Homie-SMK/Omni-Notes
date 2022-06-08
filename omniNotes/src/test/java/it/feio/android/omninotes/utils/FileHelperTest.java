36768cd7421895a242606c38e67cd9fa5a3c349b
Sungmin Kang <46037988+Homie-SMK@users.noreply.github.com>
2022-06-08 20:57:04 +0900

    [LocalUnitTest][FileHelperTest] added getFilePrefixTest for FileHelper.getFilePrefix method

---
+++ omniNotes/src/test/java/it/feio/android/omninotes/utils/FileHelperTest.java
@@ -0,0 +1,23 @@
+package it.feio.android.omninotes.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import android.os.Environment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import it.feio.android.omninotes.BaseUnitTest;
+
+public class FileHelperTest extends BaseUnitTest {
+    @Test
+    public void getFilePrefixTest() {
+        String testFileName = "test.exe";
+        String expected = "test";
+        assertEquals(FileHelper.getFilePrefix(testFileName), expected);
+    }
+}
