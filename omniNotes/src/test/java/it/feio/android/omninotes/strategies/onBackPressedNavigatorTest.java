1d5231fa75cea15c7ffac398fa468dc37b77491b
Sungmin Kang <46037988+Homie-SMK@users.noreply.github.com>
2022-06-08 23:33:03 +0900

    [LocalUnitTest][onBackPressedNavigatorTest] added processTest for onBackPressedNavigator.process

--- omniNotes/src/test/java/it/feio/android/omninotes/strategies/onBackPressedNavigatorTest.java
+++ omniNotes/src/test/java/it/feio/android/omninotes/strategies/onBackPressedNavigatorTest.java
@@ -3,20 +3,24 @@ package it.feio.android.omninotes.strategies;
 import android.os.Build;
 
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import it.feio.android.omninotes.DetailFragment;
 import it.feio.android.omninotes.ListFragment;
 import it.feio.android.omninotes.OmniNotes;
 import it.feio.android.omninotes.SketchFragment;
+import it.feio.android.omninotes.factory.FragmentStrategy;
 
-@RunWith(PowerMockRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @PrepareForTest({OmniNotes.class, Build.VERSION.class})
 @PowerMockIgnore("jdk.internal.reflect.*")
 
@@ -31,5 +35,26 @@ public class onBackPressedNavigatorTest {
         assertSame(mNavigator.getStrategy().getClass(), ListFragmentStrategy.class);
         mNavigator = new onBackPressedNavigator(SketchFragment.class);
         assertSame(mNavigator.getStrategy().getClass(), SketchFragmentStrategy.class);
+    }
+
+    @Test
+    public void processTest() {
+        mNavigator = new onBackPressedNavigator(DetailFragment.class);
+        DetailFragmentStrategy detailFragmentStrategy = mock(DetailFragmentStrategy.class);
+        doNothing().when(detailFragmentStrategy).process();
+        mNavigator.process();
+        verify(detailFragmentStrategy, times(1)).process();
+
+        mNavigator = new onBackPressedNavigator(ListFragment.class);
+        ListFragmentStrategy listFragmentStrategy = mock(ListFragmentStrategy.class);
+        doNothing().when(listFragmentStrategy).process();
+        mNavigator.process();
+        verify(listFragmentStrategy, times(1)).process();
+
+        mNavigator = new onBackPressedNavigator(SketchFragment.class);
+        SketchFragmentStrategy sketchFragmentStrategy = mock(SketchFragmentStrategy.class);
+        doNothing().when(sketchFragmentStrategy).process();
+        mNavigator.process();
+        verify(sketchFragmentStrategy, times(1)).process();
     }
 }
